661665778cd7cadf9dcef0123d58dd8812c2c1f4
oniasfilho <onias.filho@gmail.com>
2023-09-10 07:34:09 -0400

    removes zoom option

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Viewport } from 'next/dist/lib/metadata/types/extra-types'
 import { Josefin_Sans } from 'next/font/google'
 
 const josefin = Josefin_Sans({ subsets: ['latin'] })
 
+const viewportConfig: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
 export const metadata: Metadata = {
   title: 'Todo App',
   description: 'Created with love by @oniasfilho',
+  viewport: viewportConfig
 }
 
 export default function RootLayout({
